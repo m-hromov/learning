6565dc528d4d043c8b48395004213f8698978e3d
m-hromov <hromov095w@gmail.com>
2023-05-01 15:16:10 +0300

    Added user entity

--- Module-03/common/src/main/java/com/epam/esm/model/GiftCertificate.java
+++ Module-03/common/src/main/java/com/epam/esm/model/GiftCertificate.java
@@ -1,22 +1,44 @@
 package com.epam.esm.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
+@Table(name = "GIFT_CERTIFICATE")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class GiftCertificate {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String description;
+
+    @Column
     private Double price;
+
+    @Column
     private Long duration;
+
+    @Column
     private LocalDateTime createDate;
+
+    @Column
     private LocalDateTime lastUpdateDate;
+
+    @ManyToMany
+    @JoinTable(name = "GIFT_CERTIFICATE_TAG",
+            joinColumns = @JoinColumn(name = "gift_certificate_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private List<Tag> tags;
--- Module-03/common/src/main/java/com/epam/esm/model/User.java
+++ Module-03/common/src/main/java/com/epam/esm/model/User.java
@@ -1,6 +1,5 @@
 package com.epam.esm.model;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,16 +7,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "TAG")
+@Table(name = "USER_TABLE")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tag {
+public class User {
     @Id
     @GeneratedValue
     private Long id;
 
     @Column
-    private String name;
+    private String username;
+
+    @Column
+    private String password;
 }
