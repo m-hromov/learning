d13616d06a8cdb46ef7062b200e2c53e9112eb4a
m-hromov <hromov095w@gmail.com>
2023-05-29 00:45:21 +0300

    Add tests

--- Module-03/common/src/main/java/com/epam/esm/model/paging/Pageable.java
+++ Module-03/common/src/main/java/com/epam/esm/model/paging/Pageable.java
@@ -2,12 +2,14 @@ package com.epam.esm.model.paging;
 
 import jakarta.validation.constraints.Min;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Pageable {
     @Min(1)
     private int page;
