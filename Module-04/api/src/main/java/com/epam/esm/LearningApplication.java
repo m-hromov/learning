b2c09de3e51409ca5dccfa83ef44e32eb9d7013d
m-hromov <hromov095w@gmail.com>
2023-08-06 00:01:30 +0300

    Set .war packaging

--- Module-04/api/src/main/java/com/epam/esm/LearningApplication.java
+++ Module-04/api/src/main/java/com/epam/esm/LearningApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
-public class LearningApplication {
+public class LearningApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LearningApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LearningApplication.class, args);
     }
+
 }
